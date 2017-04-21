3c05b765e4ef7517cb0a7ba0e6cdec4d56e98ee0
Andre Farzat <andrefarzat@gmail.com>
2017-04-21 17:01:42 -0300

    Small improvements

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
-angular
-    .module('timesApp')
-    .controller('TimesController', function(Times, $scope, $ionicLoading) {
+(function(angular) {
+
+    var app = angular.module('timesApp');
+
+    app.controller('TimesController', function(Times, $scope, $ionicLoading) {
         var _this = this;
 
         $scope.$on('$ionicView.enter', function(){
@@ -15,29 +17,31 @@ angular
                 $ionicLoading.hide();
             });
         });
-    })
-	
-	.controller('TimeDetalheController', function(Times, $stateParams, $scope, $ionicLoading) {
+    });
+
+    app.controller('TimeDetalhesController', function(Times, $scope, $ionicLoading, $stateParams) {
         var _this = this;
-        
-        $scope.$on('$ionicView.enter', function(){
+
+        $scope.$on('$ionicView.enter', function() {
             $ionicLoading.show();
 
-            Times.getTimes().then(function(response){
-				
-           	for (var i = 0; i < response.data['results']['bindings'].length; i++) {
-				if (response.data['results']['bindings'][i]['nome']['value'] == $stateParams.timeNome) {
-					_this.nome = response.data['results']['bindings'][i]['nome']['value']; 
-					_this.escudo = response.data['results']['bindings'][i]['escudo']['value']; 				  
-					break;
-			   }   
-			}
-			
-			}).catch(function(response){
+            Times.getTimes().then(function(response) {
+
+                for (var i = 0; i < response.data['results']['bindings'].length; i++) {
+                    if (response.data['results']['bindings'][i]['nome']['value'] == $stateParams.timeNome) {
+                        _this.nome = response.data['results']['bindings'][i]['nome']['value'];
+                        _this.escudo = response.data['results']['bindings'][i]['escudo']['value'];
+                        break;
+                    }
+                }
+
+			}).catch(function(response) {
                 //request was not successful
                 //handle the error
-            }).finally(function(){
+            }).finally(function() {
                 $ionicLoading.hide();
             });
         });
     });
+
+})(angular);
